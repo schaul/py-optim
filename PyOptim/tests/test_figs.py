48ab0b30d07a2f0b0114cf7a0ba309f819f75a41
Tom Schaul <schaul@gmail.com>
2013-02-26 14:27:26 -0500

    heatmap figures

--- PyOptim/tests/test_figs.py
+++ PyOptim/tests/test_figs.py
@@ -2,20 +2,20 @@ from tools.experiments import lossTraces
 from core.datainterface import FunctionWrapper
 from benchmarks.stoch_1d import StochQuad
 from algorithms import SGD, AdaGrad, Amari, OracleSGD, RMSProp
-from tools.plotting import plotWithPercentiles, algo_colors
+from tools.plotting import plotWithPercentiles, algo_colors, plotHeatmap
 import pylab
 
-def testPlot1(dim=20):
-    f = FunctionWrapper(dim, StochQuad(noiseLevel=0.2))
-    ls = lossTraces(fwrap=f, aclass=SGD, dim=dim, maxsteps=100, algoparams={'learning_rate':0.2})
+def testPlot1(trials=20):
+    f = FunctionWrapper(trials, StochQuad(noiseLevel=0.2))
+    ls = lossTraces(fwrap=f, aclass=SGD, dim=trials, maxsteps=100, algoparams={'learning_rate':0.2})
     pylab.plot(ls, 'b:')
     pylab.plot(pylab.mean(ls, axis=1), 'r-')
     pylab.semilogy()
     pylab.show()
 
 
-def testPlot2(dim=100, maxsteps=250):
-    f = FunctionWrapper(dim, StochQuad(noiseLevel=1, curvature=10))
+def testPlot2(trials=100, maxsteps=250):
+    f = FunctionWrapper(trials, StochQuad(noiseLevel=1, curvature=1))
     for aclass, aparams in [(SGD, {'learning_rate':0.1}),
                             (SGD, {'learning_rate':0.01}),
                             (AdaGrad, {'init_lr':0.3}),
@@ -23,7 +23,7 @@ def testPlot2(dim=100, maxsteps=250):
                             (RMSProp, {'init_lr':0.1}),
                             (OracleSGD, {}),
                             ]:
-        ls = lossTraces(fwrap=f, aclass=aclass, dim=dim, 
+        ls = lossTraces(fwrap=f, aclass=aclass, dim=trials,
                         maxsteps=maxsteps, algoparams=aparams)
         plotWithPercentiles(ls, algo_colors[aclass], aclass.__name__)
     pylab.semilogy()
@@ -31,8 +31,28 @@ def testPlot2(dim=100, maxsteps=250):
     pylab.legend()
     pylab.show()
 
+def testPlot3(trials=100, maxsteps=2 ** 10):
+    fwrap = FunctionWrapper(trials, StochQuad(noiseLevel=1, curvature=1))
+    ploti = 1
+    variants = [(SGD, {'learning_rate':0.1}),
+                (SGD, {'learning_rate':0.01}),
+                (AdaGrad, {'init_lr':0.3}),
+                (Amari, {'init_lr':0.1, 'time_const':100}),
+                (RMSProp, {'init_lr':0.1}),
+                (OracleSGD, {}),
+                ]
+    ratio = 1
+    tot = len(variants)
+    rows = int(pylab.sqrt(tot) / ratio)     
+    cols = (tot + rows - 1) / rows 
+    for aclass, aparams in variants:
+        pylab.subplot(rows, cols, ploti); ploti += 1
+        plotHeatmap(fwrap, aclass, aparams, trials, maxsteps)
+        pylab.title(aclass.__name__)        
+    pylab.show()
     
     
 if __name__ == "__main__":
     #testPlot1()
-    testPlot2()
+    #testPlot2()
+    testPlot3()
--- PyOptim/tools/plotting.py
+++ PyOptim/tools/plotting.py
@@ -1,9 +1,10 @@
-from scipy import median
-from tools import percentile    
-    
-    
-from algorithms import SGD, AdaGrad, Amari, MomentumSGD, OracleSGD, RMSProp, vSGD
 import pylab
+from scipy import median, log2, mean, exp, zeros,isnan, sqrt, log10, clip
+from pylab import cm
+    
+from tools import percentile
+from experiments import lossTraces
+from algorithms import SGD, AdaGrad, Amari, MomentumSGD, OracleSGD, RMSProp, vSGD
 
 
 algo_colors = {SGD: 'b',
@@ -16,9 +17,6 @@ algo_colors = {SGD: 'b',
                }
 
 
-
-
-
 def plotWithPercentiles(ltraces, color, name=None, plotall=False):
     m = median(ltraces, axis=1)
     lp = percentile(ltraces, 25, axis=1)
@@ -28,5 +26,33 @@ def plotWithPercentiles(ltraces, color, name=None, plotall=False):
             pylab.plot(l, color + '-', alpha=0.3)
     pylab.plot(m, color + '-', label=name)
     pylab.fill_between(range(len(m)), lp, up, facecolor=color, alpha=0.1)
-    #pylab.plot(up, color + '+:')
-    #pylab.plot(lp, color + '+:')
+    
+    
+def plotHeatmap(fwrap, aclass, algoparams, trials, maxsteps):
+    """ Visualizing performance across trials and across time 
+    (iterations in powers of 2) """
+    psteps = int(log2(maxsteps)) + 1
+    storesteps = [0] + [2 ** x  for x in range(psteps)]
+    ls = lossTraces(fwrap, aclass, dim=trials, maxsteps=maxsteps,
+                    storesteps=storesteps, algoparams=algoparams,
+                    minLoss=1e-10)
+            
+    initv = mean(ls[0])
+    maxgain = exp(fwrap.stochfun.maxLogGain(maxsteps) + 1)
+    maxneggain = (sqrt(maxgain))
+    
+    M = zeros((psteps, trials))
+    for sid in range(psteps):
+        # skip the initial values
+        winfactors = clip(initv / ls[sid+1], 1. / maxneggain, maxgain)
+        winfactors[isnan(winfactors)] = 1. / maxneggain
+        M[sid, :] = log10(sorted(winfactors))
+        
+    pylab.imshow(M.T, interpolation='nearest', cmap=cm.RdBu, #@UndefinedVariable
+                 aspect=psteps / float(trials) / 1,  
+                 vmin= -log10(maxgain), vmax=log10(maxgain),
+                 )   
+    pylab.xticks([])
+    pylab.yticks([])
+    return ls
+        
