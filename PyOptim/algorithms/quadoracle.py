11f072278d0b5d655768c45cd3044652695043ea
Tom Schaul <schaul@gmail.com>
2013-02-25 19:47:31 -0500

    oracle

---
+++ PyOptim/algorithms/quadoracle.py
@@ -0,0 +1,16 @@
+from sgd import SGD
+from benchmarks.stoch_1d import StochQuad
+
+class OracleSGD(SGD):
+    """ An algorithm that cheats, because it
+    always knows the optimum learning rate (assuming a quadratic loss function) """
+    
+    def _additionalInit(self):
+        assert isinstance(self.provider.stochfun, StochQuad)
+        self._noiseLevel = self.provider.stochfun.noiseLevel
+        self._curvature = self.provider.stochfun.curvature
+    
+    @property
+    def learning_rate(self):
+        return self.parameters ** 2 / (self.parameters ** 2 + self._noiseLevel ** 2 / self.batch_size) / self._curvature          
+    
--- PyOptim/tests/test_quad.py
+++ PyOptim/tests/test_quad.py
@@ -13,15 +13,27 @@ def testWrapper(dim=5):
     print f1.currentGradients(ones(dim))
     print 
     
+def printy(s):
+    print s._num_updates, s.parameters, s.provider.currentLosses(s.parameters)
+    
 def testSGD(dim=3):
-    f = FunctionWrapper(dim, StochQuad(noiseLevel=0.1))
+    f = FunctionWrapper(dim, StochQuad(noiseLevel=0.2))
     x0 = ones(dim)
     from algorithms.sgd import SGD
-    def printy(s):
-        print s._num_updates, s.parameters, s.provider.currentLosses(s.parameters)
     algo = SGD(f, x0, callback=printy, learning_rate=0.2, loss_target=0.01)
     algo.run(100)
+    print
+    
+    
+def testOracle(dim=3):
+    from algorithms.quadoracle import OracleSGD
+    f = FunctionWrapper(dim, StochQuad(noiseLevel=0.2))
+    x0 = ones(dim)
+    algo = OracleSGD(f, x0, callback=printy, loss_target=0.01)
+    algo.run(100)
+    print
     
 if __name__ == "__main__":
     testWrapper()
     testSGD()
+    testOracle()
